d6a9410974e2ddddae3a286a9c69085a49509c99
Satoshi Tajima <tajima1989@gmail.com>
2018-09-22 21:32:57 +0900

    Solved "Problem C. Bathroom Stalls".

--- 2017/Qualification/C_BathroomStalls/solver.go
+++ 2017/Qualification/C_BathroomStalls/solver.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"strconv"
 	"strings"
 )
 
 type solver struct {
-	n int // Number of stalls.
-	k int // Number of people are about to enter the bathroom.
+	n uint64 // Number of stalls.
+	k uint64 // Number of people are about to enter the bathroom.
 }
 
 func (sol *solver) parse(str string) error {
@@ -26,52 +25,55 @@ func (sol *solver) parse(str string) error {
 		return err
 	}
 
-	sol.n = n
-	sol.k = k
+	sol.n = uint64(n)
+	sol.k = uint64(k)
 	return nil
 
 }
 
 func (sol *solver) solve() string {
-	seqs := map[int]int{sol.n: 1}
+	seqs := map[uint64]uint64{sol.n: 1}
+	rest := sol.k
 
-	max := 0
-	min := 0
+	max := uint64(0)
+	min := uint64(0)
 
-	for i := 0; i < sol.k; i++ {
-		var seq int
-		seq, seqs = selectSeq(seqs)
+	for {
+		var seq uint64
+		var num uint64
+
+		seq, num, seqs = selectSeq(seqs)
+
 		max, min = split(seq)
-		seqs[max]++
-		seqs[min]++
+		seqs[max] += num
+		seqs[min] += num
+
+		if rest <= num {
+			break
+		}
+		rest -= num
 	}
 	return fmt.Sprintf("%d %d", max, min)
 }
 
-func split(seq int) (int, int) {
-	rest := seq - 1
-
-	max := int(math.Ceil(float64(rest) / 2))
-	min := int(math.Trunc(float64(rest) / 2))
+func split(seq uint64) (uint64, uint64) {
+	max := uint64(seq / 2)
+	min := uint64((seq - 1) / 2)
 
 	return max, min
 }
 
-func selectSeq(seqs map[int]int) (int, map[int]int) {
+func selectSeq(seqs map[uint64]uint64) (uint64, uint64, map[uint64]uint64) {
 	max := findMax(seqs)
-	seqs[max]--
-
-	if seqs[max] <= 0 {
-		delete(seqs, max)
-	}
-
-	return max, seqs
+	num := seqs[max]
+	delete(seqs, max)
+	return max, num, seqs
 }
 
-func findMax(seqs map[int]int) int {
-	max := 0
-	for k, v := range seqs {
-		if k > max && v > 0 {
+func findMax(seqs map[uint64]uint64) uint64 {
+	max := uint64(0)
+	for k := range seqs {
+		if k > max {
 			max = k
 		}
 	}
